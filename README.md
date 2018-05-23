6ce0eed91cbe7b871f05c97e114e5c4590d22b4f
jaywon <jason@sudokrew.com>
2018-05-22 16:57:42 -1000

    :tada: First commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# python-os
+# Python OS
+In this module we'll explore some of the common features in Python for working with the Operating System.
+
+### Instructions
+2. **Clone** this repository from your personal Github account:
+    - Copy the HTTPS or SSH address on the page.
+    - Run the command `$ git clone [SSH or HTTP address]` in your terminal to clone this repository into your DevLeague folder 
+      in  your computer (you don't need to type the "$"; this is the command __prompt__, and is used to signify your terminal is ready for commands).
+3. From your terminal navigate to your DevLeague folder and into the `python-os` assignment:
+    - `$ cd DevLeague`
+    - `$ ls` 
+    - `cd python-os`
+4. Open the `python-os` assignment in text editor and write your code in the `exercises.py` file.
+5. Run python in your terminal to test your code:
+   - `$ cd python-os`
+   - `python exercises.py` (don't forget to print your work)
---
+++ exercises.py
@@ -0,0 +1,34 @@
+#1. Open the filenames.txt file with read-only access with the open() function
+
+#2. Print the name of the file and if it is open or closed using the .name and .closed properties
+
+#3. Use a for loop to read all lines of filenames.txt into a list variable
+
+#4. Print out all the lines from the file from your variable
+
+#5. Close the filenames.txt file and print if the file is open or closed
+
+#6. Create a file using the write() command called secrets.txt
+
+#7. Write your own secrets to the file
+
+#8. Close the secrets.txt file
+
+#9. Print out the contents of the text file
+
+#10. Open your secrets.txt file in append mode and write some more super secret info
+
+#12. Close the secrets.txt file again using the close() function
+
+#13. Make a copy of secrets.txt and make it a "hidden" file named .supersecret.txt
+
+#14. See if you can see the file in your file explorer
+
+#15. Create a list variable named file_names that contains a list of filenames
+
+#16. Use the writelines() function to append the filenames to the filenames.txt file
+
+#17. Delete the initial secrets.txt file now that you have a super secret hidden version
+
+ 
+
